0254631f7f115dde59eeccd7be5b19e81c69ea40
E-C18 <ethanc2443@gmail.com>
2026-01-31 11:12:35 -0600

    moved Forward class code to auton red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonRed.java
@@ -23,9 +23,45 @@ public class autonRed extends OpMode {
         BLM = hardwareMap.get(DcMotor.class, ("BLM"));
         FLM = hardwareMap.get(DcMotor.class, ("FLM"));
         //initialize both motors
-        moveF = new Forward();
+        //moveF = new Forward();
     }
     public void loop(){
-        moveF.moveF(305);
+        int distance = 305;
+        BRM.setTargetPosition(distance);
+        FRM.setTargetPosition(distance);
+        BLM.setTargetPosition(-distance);
+        FLM.setTargetPosition(-distance);
+        FRM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FRM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BRM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BRM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BLM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FLM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        double CPR  = 384.5;
+        double diameter = 96;
+        double circumference = Math.PI*diameter;
+        double position = FRM.getCurrentPosition();
+        double revolutions = position/CPR;
+        double distanceMoved = revolutions *circumference;
+        double power = FRM.getPower();
+        //acceleration
+        boolean end = true;
+        while (distanceMoved < distance && power < 0.75 && end) {
+            power +=0.05;
+            FRM.setPower(power);
+            FLM.setPower(-power);
+            BRM.setPower(power);
+            BLM.setPower(-power);
+            //deceleration
+            if(distance - FRM.getCurrentPosition()/CPR * circumference < 100){
+                end = false;
+            }
+        }
+        while (power > .25){
+            power -= .05;
+        }
     }
 }
+
